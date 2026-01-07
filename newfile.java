fe499f41a4f41cd1cc2a9e5dac0db136385d0fe0
SaiLokeshKesamsetty <Kesamsettysailokesh@gmail.com>
2026-01-07 09:58:15 +0530

    newjavafile

---
+++ newfile.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hello");
+        System.out.println("Good Morning");
+    }
+}
